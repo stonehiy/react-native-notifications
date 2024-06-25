5234174c3209f2454376ce3fae9e254a7a42dba8
stonehiy <34620275+stonehiy@users.noreply.github.com>
2024-06-25 13:53:10 +0800

    Update PushNotification.java

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -151,7 +151,7 @@ public class PushNotification implements IPushNotification {
                 .setContentText(mNotificationProps.getBody())
                 .setContentIntent(intent)
                 .setDefaults(Notification.DEFAULT_ALL)
-                .setAutoCancel(true);
+                .setAutoCancel(false);
 
         setUpIcon(notification);
 
